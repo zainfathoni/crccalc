3080e37b2c775285808e01273d26edf2d8017bca
Zain Fathoni <me@zainf.dev>
2023-05-01 09:06:58 +0800

    fix: add a timeout of 2000 ms

--- tests/crc-scraping.spec.js
+++ tests/crc-scraping.spec.js
@@ -1,14 +1,14 @@
 import { test, expect } from "@playwright/test";
 import { readFixture, writeFixture } from "../utils/fixtures";
 
-test("scraping", async ({ page }) => {
+test("crc-scraping", async ({ page }) => {
   const JSONstring = await readFixture("emails.local.json");
   const emails = JSON.parse(JSONstring);
 
   const hashes = new Map();
   for (const email of emails) {
     const url = `https://crccalc.com/?crc=${email}&method=crc16&datatype=ascii&outtype=0`;
-    await page.goto(url);
+    await page.goto(url, { timeout: 2000 });
     const hash = await page
       .locator("css=#crcTable > tbody > :nth-child(2) > :nth-child(2)")
       .textContent();
